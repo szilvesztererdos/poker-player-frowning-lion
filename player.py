3078719282ea51445e02030217e480757493454b
Szilveszter Erdős <szilveszter.erdos@codecool.com>
2017-01-05 14:15:39 +0100

    2x

--- player.py
+++ player.py
@@ -9,10 +9,13 @@ class Player:
         current_buy_in = game_state["current_buy_in"]
         players = game_state["players"]
         in_action = int(game_state["in_action"])
-        print(in_action)
         minimum_raise = game_state["minimum_raise"]
+        small_blind = game_state["small_blind"]
+        hole_cards = game_state["hole_cards"]
+        community_cards = game_state["community_cards"]
         
-        return current_buy_in - players[in_action]["bet"] + minimum_raise
+
+        return current_buy_in - players[in_action]["bet"] + minimum_raise * 2
 
     def showdown(self, game_state):
         pass
